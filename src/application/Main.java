65cfc41f3ae0ff95b76a243fdb42eeb7b6995999
Renan Silva <renansilvapinheiro22@gmail.com>
2025-05-01 11:39:54 -0300

    Atualização para JOptionPane

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.entities.Vehicle;
 import model.services.BrazilTaxService;
 import model.services.RentalService;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,37 +15,38 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            JOptionPane.showMessageDialog(null, "-- ENTRE COM OS DADOS DO ALUGUEL --");
+            String name = JOptionPane.showInputDialog(null, "Modelo do carro: ");
+            String startStr = JOptionPane.showInputDialog(null, "Retirada (dd/MM/yyyy hh:mm): ");
+            LocalDateTime start = LocalDateTime.parse(startStr, dtf);
+            String finishStr = JOptionPane.showInputDialog(null, "Retirada (dd/MM/yyyy hh:mm): ");
+            LocalDateTime finish = LocalDateTime.parse(finishStr, dtf);
 
-        System.out.println("-- Entre com os dados do aluguel -- ");
-        System.out.println("Modelo do carro: ");
-        String name = br.readLine();
-        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
-        LocalDateTime start = LocalDateTime.parse(br.readLine(), dtf);
-        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
-        LocalDateTime finish = LocalDateTime.parse(br.readLine(), dtf);
+            CarRental carRental = new CarRental(start, finish, new Vehicle(name));
 
+            String horaStr = JOptionPane.showInputDialog("Entre com o preço por hora:");
+            double hora = Double.parseDouble(horaStr);
 
-        CarRental carRental = new CarRental(start, finish, new Vehicle(name));
+            String diaStr = JOptionPane.showInputDialog("Entre com o preço por dia:");
+            double dia = Double.parseDouble(diaStr);
 
-        System.out.println("Entre com o preço por hora: ");
-        double hora = Double.parseDouble(br.readLine());
-        System.out.println("Entre com o preço por dia: ");
-        double dia = Double.parseDouble(br.readLine());
+            RentalService rentalService = new RentalService(hora, dia, new BrazilTaxService());
 
-        RentalService rentalService = new RentalService(hora, dia, new BrazilTaxService());
+            rentalService.processInvoice(carRental);
 
-        rentalService.processInvoice(carRental);
+            StringBuilder sb = new StringBuilder();
+            sb.append("-- FATURA --\n");
+            sb.append("Pagamento básico: R$ ").append(String.format("%.2f", carRental.getInvoice().getBasicPayment())).append("\n");
+            sb.append("Imposto: R$ ").append(String.format("%.2f", carRental.getInvoice().getTax())).append("\n");
+            sb.append("Pagamento total: R$ ").append(String.format("%.2f", carRental.getInvoice().getTotalPayment()));
 
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("-- FATURA -- ");
-        sb.append("\nPagamento básico: " + carRental.getInvoice().getBasicPayment());
-        sb.append("\nImposto: " + carRental.getInvoice().getTax());
-        sb.append("\nPagamento total: " + carRental.getInvoice().getTotalPayment());
-
-        System.out.println(sb.toString());
+            JOptionPane.showMessageDialog(null, sb.toString());
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        }
     }
 }
